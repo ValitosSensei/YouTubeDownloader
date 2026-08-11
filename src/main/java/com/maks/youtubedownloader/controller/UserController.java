07b63656b078461f9a7274859da912909ee633c5
ValitosSensei <89963241+ValitosSensei@users.noreply.github.com>
2026-08-11 12:25:47 +0200

    feat(controller): add user registration endpoint

---
+++ src/main/java/com/maks/youtubedownloader/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.maks.youtubedownloader.controller;
+
+import com.maks.youtubedownloader.dto.request.RegistrationRequest;
+import com.maks.youtubedownloader.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping("/registration")
+    public String registerNewUser(RegistrationRequest registrationRequest) {
+        userService.createNewUser(registrationRequest);
+        return "New user registered "+registrationRequest.getFirstName()+
+                " "+registrationRequest.getLastName();
+    }
+}
