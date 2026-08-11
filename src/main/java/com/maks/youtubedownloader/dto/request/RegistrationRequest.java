174f3eb93acc503372dc3f347c3b7835534c9481
ValitosSensei <89963241+ValitosSensei@users.noreply.github.com>
2026-08-11 11:43:32 +0200

    configure password encoder

---
+++ src/main/java/com/maks/youtubedownloader/dto/request/RegistrationRequest.java
@@ -0,0 +1,28 @@
+package com.maks.youtubedownloader.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegistrationRequest {
+    @Size(min = 1, max = 50)
+    @NotNull
+    private String firstName;
+
+    @Size(min = 1, max = 50)
+    private String lastName;
+
+    @Email
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    private String password;
+
+
+}
