4f7d7558a497edf4de40f655f9a0a52ca3df08eb
ValitosSensei <89963241+ValitosSensei@users.noreply.github.com>
2026-08-13 11:32:19 +0200

    feat(validation): improve registration field validation

--- src/main/java/com/maks/youtubedownloader/dto/request/RegistrationRequest.java
+++ src/main/java/com/maks/youtubedownloader/dto/request/RegistrationRequest.java
@@ -1,6 +1,7 @@
 package com.maks.youtubedownloader.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -10,18 +11,18 @@ import lombok.Setter;
 @Setter
 public class RegistrationRequest {
     @Size(min = 1, max = 50)
-    @NotNull
+    @NotBlank
     private String firstName;
 
-    @Size(max = 50)
+    @Size( max = 50)
     private String lastName;
 
     @Email
-    @NotNull
+    @NotBlank
     private String email;
 
-    @NotNull
-    @Size(min = 1, max = 50)
+    @NotBlank
+    @Size(min = 8, max = 50)
     private String password;
 
 
